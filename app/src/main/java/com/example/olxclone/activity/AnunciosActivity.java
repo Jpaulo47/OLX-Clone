c2a1f4f8b1b8e36be44afba0b8083c80d4883b30
João Paulo da Silva Rodrigues <joaopaulo.rodrigues@a4pm.com.br>
2022-11-10 22:43:22 -0300

    Adição de novas telas e funcionalidades

---
+++ app/src/main/java/com/example/olxclone/activity/AnunciosActivity.java
@@ -0,0 +1,14 @@
+package com.example.olxclone.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnunciosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anuncios);
+    }
+}
